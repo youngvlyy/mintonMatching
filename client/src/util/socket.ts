2031d13b61518dfd9fc1149f43e5bce5029c3fce
youngvlyy <qqm2308@gmail.com>
2026-02-02 21:08:57 +0900

    0202

--- client/src/util/socket.ts
+++ client/src/util/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://192.168.219.46:4000", {
+export const socket = io("http://localhost:4000", {
   autoConnect: true,
   withCredentials: true,
   transports: ["websocket","polling"],
